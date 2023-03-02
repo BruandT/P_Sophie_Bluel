30076ddfc7f3c19ee477445fde2c635b3eddb432
Thomas Bruand <thomas.bruand@gmail.com>
2023-03-02 12:52:14 +0100

    Ajout du fichier login.js, fonction de connection + recuperation du token avec un stockage dans le localStorage

---
+++ FrontEnd/login.js
@@ -0,0 +1,38 @@
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+
+const url = "http://localhost:5678/api/users/login";
+
+
+
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  const data = { email: email.value, password: password.value };
+  console.log(data)
+  if (!email.checkValidity() && !emailRegex.test(email.value)) {
+    alert("Veuillez entrer une adresse e-mail valide");
+    event.preventDefault();
+  }
+  
+  if (!password.checkValidity() && !passwordRegex.test(password.value)) {
+    alert("Veuillez entrer un mot de passe valide (8 caractères minimum, avec au moins une lettre majuscule, une lettre minuscule et un chiffre)");
+    event.preventDefault();
+  }
+
+  fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  })
+  .then(response => response.json())
+  .then(data => {
+    localStorage.setItem('token', JSON.stringify(data.token))
+    localStorage.setItem("connected", "yes")  
+    window.location.href = "index.html";
+  })
+  .catch(error => console.error(error));
+  
+  event.preventDefault();
+});
