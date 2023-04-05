13887c5b36649069fa978c8cecc413377b400536
Thomas Bruand <thomas.bruand@gmail.com>
2023-04-05 13:23:53 +0200

    Modification de la facon de se connecter, ajout de la demande serveur pour verifier que le couple email+password soit correct

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,36 +1,47 @@
-const email = document.querySelector("#email");
-const password = document.querySelector("#password");
+const form = {
+  email: document.querySelector("#email"),
+  password: document.querySelector("#password"),
+  valid: document.querySelector("#valid")
+};
+
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
 
 const url = "http://localhost:5678/api/users/login";
 
-
-
-
-document.querySelector("form").addEventListener("submit", (event) => {
-  const data = { email: email.value, password: password.value };
-  if (!email.checkValidity() && !emailRegex.test(email.value)) {
-    alert("Veuillez entrer une adresse e-mail valide");
-    event.preventDefault();
-  }
-  
-  if (!password.checkValidity() && !passwordRegex.test(password.value)) {
-    alert("Veuillez entrer un mot de passe valide (8 caractères minimum, avec au moins une lettre majuscule, une lettre minuscule et un chiffre)");
-    event.preventDefault();
-  }
-
-  fetch(url, {
+async function userLogin(email, password) {
+  const response = await fetch(url, {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
-  })
-  .then(response => response.json())
-  .then(data => {
-    localStorage.setItem("token", data.token)
-    window.location.href = "index.html";
-  })
-  .catch(error => console.error(error));
-  
-  event.preventDefault();
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  });
+  return response.json();
+}
+
+form.valid.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (!email.checkValidity() && !emailRegex.test(email.value)) {
+    alert("Veuillez entrer une adresse e-mail valide")
+  }if (!password.checkValidity() && !passwordRegex.test(password.value)) {
+    alert("Veuillez entrer un mot de passe valide");
+  } else {
+    userLogin(form.email.value, form.password.value)
+    .then((data) => {
+      if (!data.userId) {
+        alert("Erreur dans l'identifiant ou le mot de passe");
+      } else {
+        localStorage.setItem("token", data.token)
+        window.location.href = "index.html";
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+  }   
 });
