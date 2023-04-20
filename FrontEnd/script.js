187081f16893438db36fdeb09e66863e92e864c5
Thomas Bruand <thomas.bruand@gmail.com>
2023-04-20 18:44:12 +0200

    ajout de commentaire dans le fichier script.js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -3,16 +3,15 @@ const urlWorks = `${url}/works`;
 const imagesContainer = document.querySelector('.gallery');
 const galleryModal = document.querySelector('#gallery-main');
 
+
 window.onload = ()=>{
     sessionStorage.setItem('ids', JSON.stringify([]))
-    sessionStorage.removeItem("idDeleted")
-
 }
 
-// reset les galeries principale et modale
+// Reset gallery (imagesContainer, galleryModal)
 resetGallery();
 
-// appel les works de l'api
+// Recuperation des projets depuis l'api
 async function getData(urlWorks){
     await fetch(urlWorks)
     .then(response => {
@@ -25,9 +24,8 @@ async function getData(urlWorks){
             createElement(data[i]);
             createElementModal(data[i]);
 
-            // pour test le delete
+            // Ajout des id dans le sessionStorage pour les utiliser pour la suppression
             const ids = data.map(item => item.id);
-            // Convertit le tableau d'IDs en chaîne de caractères JSON
             const idsString = JSON.stringify(ids);
             sessionStorage.setItem('ids', idsString);
 
@@ -35,16 +33,18 @@ async function getData(urlWorks){
         
         const liItem = document.querySelectorAll('#tri li');
         const figureItem = document.querySelectorAll('.gallery figure');
-        // partie filtre
+
+        // Filtre
         liItem.forEach (li => {
             li.onclick = function(){    
-                // changement de class pour le clique (changement de style)
+                // Lors du clique, enleve la classe a tous les <li>
                 liItem.forEach(li => {
                     li.className = "";
                 })
+                // Cliquer sur le <li> lui ajoute la classe
                 li.className = "tri-option";          
                 
-                // filtre
+                // Compare les valeurs de categorie entre l'api et le html
                 const valueCategories = li.getAttribute('data-categories');
                 figureItem.forEach(figure => {
                     figure.style.display = 'none';
@@ -58,63 +58,73 @@ async function getData(urlWorks){
     })
 }
 
+// Appel de la function
 getData(urlWorks);
 
-// element de gallery
+// Element de la galerie
 function createElement(data){
-    // creation des differents elements
+
+    // Creation de <figure>
     const figure = document.createElement('figure');
-    const figureCaption = document.createElement('figcaption');
-    const figureImage = document.createElement('img');
-    
-    // ajout des valeurs de l'objet 
-    figureImage.src = data.imageUrl;
-    figureImage.alt = data.title;
-    figureCaption.innerHTML = data.title;
     figure.setAttribute('data-category', data.categoryId);
     figure.setAttribute('data-id', data.id);
-    
-    // liaison des enfants et des parents
     imagesContainer.appendChild(figure);
-    figure.appendChild(figureImage);
+
+    // Creation de <figcaption>
+    const figureCaption = document.createElement('figcaption');
+    figureCaption.innerHTML = data.title;
     figure.appendChild(figureCaption);
+
+    // Creation de <img>
+    const figureImage = document.createElement('img');
+    figureImage.src = data.imageUrl;
+    figureImage.alt = data.title;
+    figure.appendChild(figureImage);   
 }
-// element de modal gallery
+
+// Element de la modale
 function createElementModal(data){
-    //creation des differents elements
+
+    // Creation de la <div> pour les projets dans la modale
     const divCard = document.createElement('div');
-    const buttonEditCard = document.createElement('button');
-    const divImage = document.createElement('img');
-    const deleteDivCard = document.createElement('i');
-    const moveDivCard = document.createElement('i');
-    
-    // ajout des valeurs de l'objet 
-    divImage.src = data.imageUrl;
-    buttonEditCard.innerHTML = "éditer";
     divCard.setAttribute('data-category', data.categoryId);
     divCard.setAttribute('id', data.id);
-    
-    // ajout des classes aux elements
     divCard.className = 'gallery-card';
+    galleryModal.appendChild(divCard);
+
+    // Creation de <img>
+    const divImage = document.createElement('img');
+    divImage.src = data.imageUrl;
+    divCard.appendChild(divImage);
+
+    // Creation de <button> pour edite (non fonctionnel)
+    const buttonEditCard = document.createElement('button');
+    buttonEditCard.innerHTML = "éditer";
     buttonEditCard.className = 'button-edit-img-modal';
+    divCard.appendChild(buttonEditCard);
+
+    // Creation de <i> pour le delete la <div>
+    const deleteDivCard = document.createElement('i');
     deleteDivCard.className = 'fa-trash-can';
     deleteDivCard.classList.add('fas', 'fa-trash-alt');
+    divCard.appendChild(deleteDivCard);
+
+    // Creation de <i> pour deplacer la <div> (non fonctionnel)
+    const moveDivCard = document.createElement('i');
     moveDivCard.className = 'fa-arrows-up-down-left-right';
     moveDivCard.classList.add('fa-solid', 'fa-arrows-up-down-left-right');
-    
-    // liaison des enfants et des parents
-    galleryModal.appendChild(divCard);
-    divCard.appendChild(divImage);
-    divCard.appendChild(buttonEditCard);
-    divCard.appendChild(deleteDivCard);
     divCard.appendChild(moveDivCard);
 
 
-    // ajout de l'écouteur d'événements sur l'icône de poubelle
-    deleteDivCard.addEventListener('click', ()=>{   
+
+    // Ajout de l'ecouteur d'evenements sur l'icone de poubelle
+    deleteDivCard.addEventListener('click', ()=>{ 
+        // Remove le projet dans (imagesContainer) 
         document.querySelector(`[data-id="${data.id}"]`).remove()
+        // Remove le projet dans (galleryModal)
         document.querySelector(`[id="${data.id}"]`).remove()  
 
+        // Suppression du projet depuis l'API
         fetch(`http://localhost:5678/api/works/${data.id}`, {
                     method: "DELETE",
                     headers: {
@@ -129,12 +139,15 @@ function createElementModal(data){
     
 }
 
+// Reset les galeries
 function resetGallery(){
-    // reset la classe gallery
-    document.querySelector('.gallery').innerHTML = " ";
-    document.querySelector('#gallery-main').innerHTML = " ";
+    imagesContainer.innerHTML = " ";
+    galleryModal.innerHTML = " ";
 }
-// clear modal
+
+
+
+// Reinitialisation des modales
 function clearModal(){
     const infoTitle = document.getElementById("info-title");
     const infoImg = document.getElementById("info-img");
@@ -150,25 +163,27 @@ function clearModal(){
     document.getElementById("picture-title").value ="";
 }
 
-// ajout de photo dans la modal
+
+// Ajout de la photo dans la modale
 const inputPicture = document.getElementById("picture");
 const cardPicture = document.getElementById("before-add");
 const divPicture = document.getElementById("picture-add-card");
 
-// ajouter un écouteur d'événement sur l'élément input
+// Ajout de l'ecouteur d'evenements sur (inputPicture)
 inputPicture.addEventListener("change", function(event) {
     event.preventDefault()
-    // récupérer le fichier sélectionné
+
+    // Recupere le fichier
     const file = inputPicture.files[0];
-    // vérifier que le fichier est une image
+    // Verifie que le fichier est une image
     if (file.type.startsWith("image/") && (file.type === "image/jpeg" || file.type === "image/png")) {
-        // creer un element img
+        // Creer un element img
         let imgInput = document.createElement('img');
         imgInput.setAttribute("id", "img-input")
         divPicture.appendChild(imgInput)
-        // créer un objet URL pour afficher l'image
+        // Creer un objet URL pour afficher l'image
         const imageUrl = URL.createObjectURL(file);    
-        // remplacer le contenu de la div avec l'image        
+        // Remplace le contenu de la div avec l'image        
         imgInput.src = imageUrl;        
         cardPicture.style.display = "none";
         imgInput.style.display = "flex";        
@@ -179,21 +194,22 @@ inputPicture.addEventListener("change", function(event) {
 });
 
 
-// open modal projet
+// Ouvre la modale "Galerie photo"
 const openModalProjet = document.querySelector("#change-portfolio");
 const modalProjet = document.querySelector("#gallery-modal");
 openModalProjet.addEventListener('click', ()=>{
     modalProjet.style.display = "flex";
 })
 
-// open modal picture
+// Ouvre la modale "Ajout photo"
 const openModalPicture = document.querySelector("#open-picture-modal");
 const modalPicture = document.querySelector("#picture-modal");
 openModalPicture.addEventListener('click', ()=>{
     modalPicture.style.display = "flex";
     modalProjet.style.display = "none";
 })
-// close modal
+
+// Function pour fermer les modales
 function closeModal(close, modal){
     let closeModal = document.querySelector(close);
     let modalDisplay = document.querySelector(modal);
@@ -207,7 +223,7 @@ closeModal('#close-gallery-modal', '#gallery-modal');
 closeModal('#close-picture-modal', '#picture-modal');
 
 
-// tout supprimer gallery
+// Supprime tous les projets du site
 const deleteModalGallery = document.querySelector("#delete-gallery");
 const modalGallery = document.querySelector("#gallery-main");
 deleteModalGallery.addEventListener('click', (e)=>{
@@ -216,11 +232,11 @@ deleteModalGallery.addEventListener('click', (e)=>{
 })
 const deleteAll = () => {  
     let idDelete = JSON.parse(sessionStorage.getItem('ids')) || [];
-    // Vérifier si le tableau contient des éléments
+    // Verifie si le tableau contient des elements
     if (idDelete.length > 0) {
-        // Itérer sur chaque élément du tableau
+        // Execute l'envoi API tant que le tableau n'est pas vide
         idDelete.forEach((id) => {
-            // Envoyer une requête DELETE pour supprimer l'élément correspondant du serveur
+            // Envoie une requete DELETE pour supprimer l'element correspondant de l'API
             fetch(`http://localhost:5678/api/works/${id}`, {
                 method: "DELETE",
                 headers: {
@@ -236,18 +252,20 @@ const deleteAll = () => {
     resetGallery();
 };
 
-//connexion + editor
+// Ajout des options Admin
 const token = localStorage.getItem('token');
 const logOut = document.querySelector('#log-out');
 const logIn = document.querySelector('#log-in');
 const editor = document.querySelector('#editor');
 const editButton = document.querySelectorAll('.edit');
+// Verifie si le (token) est valide, pour l'ajout des differents <button>
 if(token){
     logOut.style.display = "block";
     logIn.style.display = "none";
     editor.style.display = "flex";
     editButton.forEach(button => button.style.display = "block");
 }else{
+    // Sinon, cache les <button>
     logOut.style.display = "none";
     logIn.style.display = "block";
     editor.style.display = "none";
@@ -255,11 +273,9 @@ if(token){
 } 
 
 
-
-// formulaire d'envoi
 const form = document.getElementById("myForm");
 
-// fonction verification des champs
+// Function verifie les champs du (form)
 const colorValid = document.querySelector("#valid-add-picture");
 function checkItems(e) {
     e.preventDefault();
@@ -293,21 +309,29 @@ function checkItems(e) {
         colorValid.disabled = true;        
     }
 }
-// event verification des champs
 form.addEventListener("change", checkItems);
 
-// creation dans le dom
+// Creation des projets lors du submit du (form)
 form.addEventListener("submit", async (e) => {    
     e.preventDefault();
+
     const title = document.getElementById("picture-title");
     const category = document.getElementById("categories-picture");
-    const imageAdd = document.getElementById("picture");
-    const figure = document.createElement('figure');
-    const figureCaption = document.createElement('figcaption');
-    const figureImage = document.createElement('img');
-    const divImage = document.createElement('img');
+    const imageAdd = document.getElementById("picture");  
 
-    // input file 
+    // Creation de <figure>
+    const figure = document.createElement('figure');
+    figure.setAttribute('data-category', category.value);
+    imagesContainer.appendChild(figure);
+
+    // Creation de <figcaption>
+    const figureCaption = document.createElement('figcaption');
+    figureCaption.innerHTML = title.value;
+    figure.appendChild(figureCaption);
+    
+    // Creation de <img> + ajout de l'image
+    const figureImage = document.createElement('img');
+    const divImage = document.createElement('img'); 
     const file = imageAdd.files[0];
     const reader = new FileReader();
     reader.onload = (event) => {
@@ -315,47 +339,43 @@ form.addEventListener("submit", async (e) => {
         divImage.src = event.target.result; 
     };
     reader.readAsDataURL(file);
-    
     figureImage.alt = title.value;
-    figureCaption.innerHTML = title.value;
-    figure.setAttribute('data-category', category.value);
-        
-    imagesContainer.appendChild(figure);
     figure.appendChild(figureImage);
-    figure.appendChild(figureCaption);
     
-    const divCard = document.createElement('div');
-    const buttonEditCard = document.createElement('button');
-    const deleteDivCard = document.createElement('i');
-    const moveDivCard = document.createElement('i');
     
-    // ajout des valeurs de l'objet 
-    buttonEditCard.innerHTML = "éditer";
+    // Creation de la <div> pour les projets dans la modale
+    const divCard = document.createElement('div');  
     divCard.setAttribute('data-category', category.value);
     divCard.style.display = "flex";
-    
-    // ajout des classes aux elements
     divCard.className = 'gallery-card';
-    buttonEditCard.className = 'button-edit-img-modal';
-    deleteDivCard.className = 'fa-trash-can';
-    deleteDivCard.classList.add('fas', 'fa-trash-alt');
-    moveDivCard.className = 'fa-arrows-up-down-left-right';
-    moveDivCard.classList.add('fa-solid', 'fa-arrows-up-down-left-right');
-
-    // liaison des enfants et des parents
     galleryModal.appendChild(divCard);
     divCard.appendChild(divImage);
+    
+    // Creation de <button> pour edite (non fonctionnel)
+    const buttonEditCard = document.createElement('button');
+    buttonEditCard.innerHTML = "éditer";
+    buttonEditCard.className = 'button-edit-img-modal';
     divCard.appendChild(buttonEditCard);
+
+    // Creation de <i> pour le delete la <div>
+    const deleteDivCard = document.createElement('i');
+    deleteDivCard.className = 'fa-trash-can';
+    deleteDivCard.classList.add('fas', 'fa-trash-alt');
     divCard.appendChild(deleteDivCard);
+
+    // Creation de <i> pour deplacer la <div> (non fonctionnel)
+    const moveDivCard = document.createElement('i');
+    moveDivCard.className = 'fa-arrows-up-down-left-right';
+    moveDivCard.classList.add('fa-solid', 'fa-arrows-up-down-left-right');
     divCard.appendChild(moveDivCard);
 
-   // Créer un nouvel objet FormData pour chaque élément
+   // Creation du formData
    let formData = new FormData();
    formData.append("title", title.value);
    formData.append("category", category.value);
    formData.append("image", imageAdd.files[0]);
 
-    // envois a l'api par rapport au nombre de formData dans le formDataArray
+    // Envoi du (formData) a l'API
         
         fetch("http://localhost:5678/api/works", {
         method: "POST",
@@ -371,26 +391,31 @@ form.addEventListener("submit", async (e) => {
         }
     })
     .then((data) =>{
+        // Remet la valeur du <select> sur une <option> vide
         document.getElementById("categories-picture").value = "";
+        // Recupere l'id de data
         const id = data.id;
+        // Attribut (id) aux elements
         divCard.setAttribute('id', id);
         figure.setAttribute('data-id', id);
+        // Envois le (id) au sessionStorage pour la recuperer lors de la suppression
         const pushStorage = JSON.parse(sessionStorage.getItem("ids"));
         pushStorage.push(id);
         sessionStorage.setItem("ids", JSON.stringify(pushStorage));
-        console.log("requete POST passé", data);
+
         const liItem = document.querySelectorAll('#tri li');
         const figureItem = document.querySelectorAll('.gallery figure');
-        // partie filtre
+        // Filtre
         liItem.forEach (li => {
             li.onclick = function(){    
-                // changement de class pour le clique (changement de style)
+                // Lors du clique, enleve la classe a tous les <li>
                 liItem.forEach(li => {
                     li.className = "";
                 })
+                // Cliquer sur le <li> lui ajoute la classe
                 li.className = "tri-option";          
                 
-                // filtre
+                // Compare les valeurs de categorie entre l'api et le html
                 const valueCategories = li.getAttribute('data-categories');
                 figureItem.forEach(figure => {
                     figure.style.display = 'none';
@@ -400,12 +425,14 @@ form.addEventListener("submit", async (e) => {
                 })
             }    
         })
-         // ajout de l'écouteur d'événements sur l'icône de poubelle
+    // Ajout de l'ecouteur d'evenements sur l'icone de poubelle
     deleteDivCard.addEventListener('click', ()=>{  
-         
+        // Remove le projet dans (imagesContainer) 
         document.querySelector(`[data-id="${data.id}"]`).remove()
+        // Remove le projet dans (galleryModal)
         document.querySelector(`[id="${data.id}"]`).remove()  
 
+        // Suppression du projet depuis l'API
         fetch(`http://localhost:5678/api/works/${data.id}`, {
                     method: "DELETE",
                     headers: {
@@ -426,7 +453,7 @@ form.addEventListener("submit", async (e) => {
 });
 
 
-// back to modal projet
+// Retourner sur la modale "Galerie photo"
 const imgInputModal = document.querySelector("#img-input")
 const backToProject = document.querySelector("#back-to-modal");
 backToProject.addEventListener('click', ()=>{  
@@ -436,7 +463,7 @@ backToProject.addEventListener('click', ()=>{
     clearModal()
     
 })
-// bouton logOut
+// <button> pour se deconnecter
 logOut.addEventListener('click', () =>{
     localStorage.removeItem('token');
     if (token) {
